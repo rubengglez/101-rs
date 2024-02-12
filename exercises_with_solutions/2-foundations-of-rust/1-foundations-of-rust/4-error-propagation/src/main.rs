38cb92c011f973d9aa4a68add2894db245eedc66
rubengglez <rubengglez@gmail.com>
2024-02-12 19:08:36 +0100

    fix error handling

--- exercises_with_solutions/2-foundations-of-rust/1-foundations-of-rust/4-error-propagation/src/main.rs
+++ exercises_with_solutions/2-foundations-of-rust/1-foundations-of-rust/4-error-propagation/src/main.rs
@@ -19,32 +19,39 @@ use std::io::{self, BufRead, BufReader, Lines};
 
 //change this into:
 //fn read_lines(filename: &str) -> Result<Lines<BufReader<File>>, io::Error> {
-fn read_lines(filename: &str) -> Lines<BufReader<File>> {
-    let file = File::open(filename).unwrap(); // this can easily fail
-    BufReader::new(file).lines()
+fn read_lines(filename: &str) -> Result<Lines<BufReader<File>>, io::Error> {
+    let file = File::open(filename)?; // this can easily fail
+    Ok(BufReader::new(file).lines())
 }
 
 //change this into:
 //fn count_bytes_and_lines(filename: &str) -> Result<(usize, usize, usize), io::Error> {
-fn count_bytes_and_lines(filename: &str) -> (usize, usize, usize) {
-    let lines = read_lines(filename);
+fn count_bytes_and_lines(filename: &str) -> Result<(usize, usize, usize), io::Error> {
+    let lines = read_lines(filename)?;
     let mut line_count = 0;
     let mut word_count = 0;
     let mut byte_count = 0;
     for line in lines {
-        let text = line.unwrap(); // this will usually not fail
-        line_count += 1;
-        word_count += text.split_whitespace().count();
-        byte_count += text.len();
+        let _ = line.map(|text| {
+            line_count += 1;
+            word_count += text.split_whitespace().count();
+            byte_count += text.len();
+        });
     }
 
-    (line_count, word_count, byte_count)
+    Ok((line_count, word_count, byte_count))
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
 
-    let (lines, words, bytes) = count_bytes_and_lines(filename);
-    println!("{filename}: {lines} lines, {words} words, {bytes} bytes");
+    let result = count_bytes_and_lines(filename);
+
+    if result.is_err() {
+        eprintln!("{}", result.err().unwrap());
+    } else {
+        let (lines, words, bytes) = result.unwrap();
+        println!("{filename}: {lines} lines, {words} words, {bytes} bytes");
+    }
 }
